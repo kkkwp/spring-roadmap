5753dc5d31fb4969d075d5d68e0aa1074cf6d0f8
kkkwp <chxrryda@gmail.com>
2023-07-14 01:45:42 +0900

    회원 관리 예제 - 회원 서비스 개발

---
+++ spring-introduction/src/main/java/hello/springintroduction/service/MemberService.java
@@ -0,0 +1,41 @@
+package hello.springintroduction.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import hello.springintroduction.domain.Member;
+import hello.springintroduction.repository.MemberRepository;
+import hello.springintroduction.repository.MemoryMemberRepository;
+
+public class MemberService {
+
+	private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+	/**
+	 * 회원 가입
+	 */
+	public Long join(Member member) {
+
+		validateDuplicateMember(member); // 중복 회원 검증
+		memberRepository.save(member);
+		return member.getId();
+	}
+
+	private void validateDuplicateMember(final Member member) {
+		memberRepository.findByName(member.getName())
+			.ifPresent(m -> {
+				throw new IllegalStateException("이미 존재하는 회원입니다.");
+			});
+	}
+
+	/**
+	 * 전체 회원 조회
+	 */
+	public List<Member> findMembers() {
+		return memberRepository.findAll();
+	}
+
+	public Optional<Member> findOne(Long memberId) {
+		return memberRepository.findById(memberId);
+	}
+}
