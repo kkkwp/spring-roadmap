736c3f6cd25bd79e533073b4cbfc62d1a9aad493
kkkwp <chxrryda@gmail.com>
2023-07-14 00:29:16 +0900

    회원 관리 예제 - 회원 도메인과 리포지토리 만들기

---
+++ spring-introduction/src/main/java/hello/springintroduction/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package hello.springintroduction.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import hello.springintroduction.domain.Member;
+
+public interface MemberRepository {
+
+	Member save(Member member);
+
+	Optional<Member> findById(Long id);
+
+	Optional<Member> findByName(String name);
+
+	List<Member> findAll();
+}
