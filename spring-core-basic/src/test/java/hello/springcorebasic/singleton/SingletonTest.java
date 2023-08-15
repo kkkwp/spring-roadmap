03a6a66ffbad0f4e39fc7182fc7adf7663f4ae6c
kkkwp <chxrryda@gmail.com>
2023-08-15 15:29:00 +0900

    싱글톤 컨테이너 - 싱글톤 패턴

--- spring-core-basic/src/test/java/hello/springcorebasic/singleton/SingletonTest.java
+++ spring-core-basic/src/test/java/hello/springcorebasic/singleton/SingletonTest.java
@@ -28,4 +28,24 @@ class SingletonTest {
 		// memberService1 != memberService2
 		assertThat(memberService1).isNotSameAs(memberService2);
 	}
+
+	@Test
+	@DisplayName("싱글톤 패턴을 적용한 객체 사용")
+	void singletonServiceTest() {
+		// private으로 생성자를 막아두었기 때문에 컴파일 오류가 발생
+		// new SingletonService();
+
+		// 조회: 호출할 때마다 같은 객체를 반환
+		SingletonService singletonService1 = SingletonService.getInstance();
+		SingletonService singletonService2 = SingletonService.getInstance();
+
+		// 참조값이 같은 것을 확인
+		System.out.println("singletonService1=" + singletonService1);
+		System.out.println("singletonService2=" + singletonService2);
+
+		// singletonService1 == singletonService2
+		assertThat(singletonService1).isSameAs(singletonService2);
+
+		singletonService1.logic();
+	}
 }
