3f1b382290c92d729b0fdf9d71af2fd191157ec8
kkkwp <chxrryda@gmail.com>
2024-06-05 11:51:00 +0900

    3. 회원 관리 예제 - 백엔드 개발: 회원 리포지토리 테스트 케이스 작성

---
+++ spring-introduction/src/test/java/com/example/springintroduction/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.example.springintroduction.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import com.example.springintroduction.domain.Member;
+
+class MemoryMemberRepositoryTest {
+
+	MemoryMemberRepository repository = new MemoryMemberRepository();
+
+	@AfterEach
+	public void afterEach() {
+		repository.clearStore();
+	}
+
+	@Test
+	void save() {
+		// given
+		Member member = new Member();
+		member.setName("spring");
+
+		// when
+		repository.save(member);
+
+		// then
+		Member result = repository.findById(member.getId()).get();
+		assertThat(result).isEqualTo(member);
+	}
+
+	@Test
+	void findByName() {
+		// given
+		Member member1 = new Member();
+		member1.setName("spring1");
+		repository.save(member1);
+
+		Member member2 = new Member();
+		member2.setName("spring2");
+		repository.save(member2);
+
+		// when
+		Member result = repository.findByName("spring1").get();
+
+		// then
+		assertThat(result).isEqualTo(member1);
+	}
+
+	@Test
+	void findByAll() {
+		// given
+		Member member1 = new Member();
+		member1.setName("spring1");
+		repository.save(member1);
+
+		Member member2 = new Member();
+		member2.setName("spring2");
+		repository.save(member2);
+
+		// when
+		List<Member> result = repository.findAll();
+
+		// then
+		assertThat(result.size()).isEqualTo(2);
+	}
+}
