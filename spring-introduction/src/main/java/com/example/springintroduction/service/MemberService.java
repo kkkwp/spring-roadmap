66356b5ad34d8eccd00def8a95d740a1e6908577
kkkwp <chxrryda@gmail.com>
2024-06-05 11:57:31 +0900

    3. 회원 관리 예제 - 백엔드 개발: 회원 서비스 개발

---
+++ spring-introduction/src/main/java/com/example/springintroduction/service/MemberService.java
@@ -0,0 +1,40 @@
+package com.example.springintroduction.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.springintroduction.domain.Member;
+import com.example.springintroduction.repository.MemberRepository;
+import com.example.springintroduction.repository.MemoryMemberRepository;
+
+public class MemberService {
+
+	private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+	/**
+	 * 회원가입
+	 */
+	public Long join(Member member) {
+		validateDuplicateMember(member); // 중복 회원 검증
+		memberRepository.save(member);
+		return member.getId();
+	}
+
+	private void validateDuplicateMember(Member member) {
+		memberRepository.findByName(member.getName())
+			.ifPresent(m -> {
+				throw new IllegalStateException("이미 존재하는 회원입니다.");
+			});
+	}
+
+	/**
+	 * 전체 회원 조회
+	 */
+	public List<Member> findMembers() {
+		return memberRepository.findAll();
+	}
+
+	public Optional<Member> findOne(Long memberId) {
+		return memberRepository.findById(memberId);
+	}
+}
