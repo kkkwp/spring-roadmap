4cea51064ab2c8efb9050e78049e606ab35a1285
kkkwp <chxrryda@gmail.com>
2024-08-13 01:06:43 +0900

    5. 회원 관리 예제 - 웹 MVC 개발: 조회

--- spring-introduction/src/main/java/com/example/springintroduction/controller/MemberController.java
+++ spring-introduction/src/main/java/com/example/springintroduction/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.example.springintroduction.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -29,5 +32,12 @@ public class MemberController {
 		member.setName(form.getName());
 		memberService.join(member);
 		return "redirect:/";
+	}
+
+	@GetMapping("/members")
+	public String list(Model model) {
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members", members);
+		return "members/memberList";
 	}
 }
