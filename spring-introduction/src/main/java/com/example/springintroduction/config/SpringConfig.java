02349cb740e27094c8e6719c476b9d9b40ac7853
kkkwp <chxrryda@gmail.com>
2024-08-13 03:23:42 +0900

    6. 스프링 DB 접근 기술: 순수 JDBC

--- spring-introduction/src/main/java/com/example/springintroduction/config/SpringConfig.java
+++ spring-introduction/src/main/java/com/example/springintroduction/config/SpringConfig.java
@@ -1,14 +1,22 @@
 package com.example.springintroduction.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.springintroduction.repository.JdbcMemberRepository;
 import com.example.springintroduction.repository.MemberRepository;
-import com.example.springintroduction.repository.MemoryMemberRepository;
 import com.example.springintroduction.service.MemberService;
 
 @Configuration
 public class SpringConfig {
+
+	private final DataSource dataSource;
+
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	@Bean
 	public MemberService memberService() {
@@ -17,6 +25,7 @@ public class SpringConfig {
 
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+		// return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
 	}
 }
